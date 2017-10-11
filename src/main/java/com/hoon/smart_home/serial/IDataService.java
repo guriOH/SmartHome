f9850030e1ead82cd977a27402febed36e9f1026
Oh Hoon <stockhoon5@gmail.com>
2017-10-12 00:33:59 +0900

    Revert "Initial commit"
    This reverts commit aece98e2351628f1ef3919ae0544e8744fe2db0c.

--- src/main/java/com/hoon/smart_home/serial/IDataService.java
+++
@@ -1,8 +0,0 @@
-package com.hoon.smart_home.serial;
-
-import com.hoon.smart_home.common.exeception.AnalysisException;
-
-public interface IDataService {
-	public void start() throws AnalysisException;
-	public void initialize() throws AnalysisException;
-}
