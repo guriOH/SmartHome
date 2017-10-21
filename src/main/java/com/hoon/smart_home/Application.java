ce37bfdbc35fd454d5a3e3973c9ac5e3b4de7d7f
Oh Hoon <stockhoon5@gmail.com>
2017-10-21 23:54:11 +0900

    When socket does not connect client, check pc windows shild

--- src/main/java/com/hoon/smart_home/Application.java
+++ src/main/java/com/hoon/smart_home/Application.java
@@ -15,26 +15,5 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		//startService();
-	}
-
-	public static boolean startService() {
-		boolean l_result = true;
-		IDataService l_service = null;
-
-		String l_serviceName = "com.hoon.smart_home.serial.socket.SocketService";
-		try {
-			if (l_serviceName != null && l_serviceName.length() > 0) {
-				l_service = (IDataService) Class.forName(l_serviceName).newInstance();
-				l_service.initialize();
-				l_service.start();
-				 logger.info("Service is started!!");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			l_result = false;
-		}
-		return l_result;
-
 	}
 }
